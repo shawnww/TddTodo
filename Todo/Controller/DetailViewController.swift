5a6d66d8504d5b4da832541286a2a8fcfea9aeaf
Shawn Wu <shawn.wu@sparkventures.co.nz>
2016-09-19 16:24:13 +1200

    Tdd DetailViewController

---
+++ Todo/Controller/DetailViewController.swift
@@ -0,0 +1,72 @@
+//
+//  DetailViewController.swift
+//  Todo
+//
+//  Created by Shawn Wu on 19/09/16.
+//  Copyright © 2016 Spark. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let dateFormatter: NSDateFormatter = {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+    
+    var itemInfo: (ItemManager, Int)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let itemInfo = itemInfo else { return }
+        let item = itemInfo.0.itemAtIndex(itemInfo.1)
+        
+        titleLabel.text = item.title
+        locationLabel.text = item.location?.name
+        descriptionLabel.text = item.itemDescription
+        
+        if let timestamp = item.timeStamp {
+            let date = NSDate(timeIntervalSince1970: timestamp)
+            dateLabel.text = dateFormatter.stringFromDate(date)
+        }
+        
+        if let coordinate = item.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coordinate,
+                                                            100, 100)
+            mapView.region = region
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    func checkItem() {
+        if let itemInfo = itemInfo {
+            itemInfo.0.checkItemAtIndex(itemInfo.1)
+        }
+    }
+    
+
+
+}
